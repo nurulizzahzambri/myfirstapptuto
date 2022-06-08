31c957408b1ab188686b8415587aaca45490fc49
nurulizzahzambri <106502769+nurulizzahzambri@users.noreply.github.com>
2022-06-08 15:12:28 +0800

    Update sampleapp.py

--- sampleapp.py
+++ sampleapp.py
@@ -9,3 +9,11 @@ chart_data = pd.DataFrame(
      columns=['a', 'b', 'c'])
 
 st.line_chart(chart_data)
+
+st.header("My first Streamlit App")
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
