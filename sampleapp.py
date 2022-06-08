1dd4142faf2f75ed712dcb4f859e4e68af7e0a6c
nurulizzahzambri <106502769+nurulizzahzambri@users.noreply.github.com>
2022-06-08 15:25:05 +0800

    Update sampleapp.py

--- sampleapp.py
+++ sampleapp.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.header("My first Streamlit App")
+st.header("Random line chart")
 
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
@@ -10,7 +10,7 @@ chart_data = pd.DataFrame(
 
 st.line_chart(chart_data)
 
-st.header("My first Streamlit App")
+st.header("Random map")
 
 map_data = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
@@ -18,7 +18,7 @@ map_data = pd.DataFrame(
 
 st.map(map_data)
 
-st.header("My first Streamlit App")
+st.header("Checkbox")
 
 st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
 show = st.checkbox('I agree the terms and conditions')
@@ -27,3 +27,36 @@ if show:
     'Intplan': ['yes', 'yes', 'yes', 'no'],
     'Churn Status': [0, 0, 0, 1]
 }))
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.header("Select box")
+
+option = st.sidebar.selectbox(
+    'Select a mini project',
+     ['line chart','map','T n C'])
+
+if option=='line chart':
+    chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+    st.line_chart(chart_data)
+
+elif option=='map':
+    map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+    st.map(map_data)
+
+else:
+    st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
+    show = st.checkbox('I agree the terms and conditions')
+    if show:
+        st.write(pd.DataFrame({
+        'Intplan': ['yes', 'yes', 'yes', 'no'],
+        'Churn Status': [0, 0, 0, 1]
+        }))
