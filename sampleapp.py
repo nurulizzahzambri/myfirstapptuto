023e77269b78248059afa16207b7cc0fba245341
nurulizzahzambri <106502769+nurulizzahzambri@users.noreply.github.com>
2022-06-08 15:25:39 +0800

    Update sampleapp.py

--- sampleapp.py
+++ sampleapp.py
@@ -2,36 +2,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.header("Random line chart")
-
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
-
-st.line_chart(chart_data)
-
-st.header("Random map")
-
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
-
-st.map(map_data)
-
-st.header("Checkbox")
-
-st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
-show = st.checkbox('I agree the terms and conditions')
-if show:
-    st.write(pd.DataFrame({
-    'Intplan': ['yes', 'yes', 'yes', 'no'],
-    'Churn Status': [0, 0, 0, 1]
-}))
-
-import streamlit as st
-import numpy as np
-import pandas as pd
-
 st.header("Select box")
 
 option = st.sidebar.selectbox(
