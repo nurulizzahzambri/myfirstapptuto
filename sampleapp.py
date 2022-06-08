37a21ed791b51e12ea73e0f31774cf1c39fd01fa
nurulizzahzambri <106502769+nurulizzahzambri@users.noreply.github.com>
2022-06-08 15:23:07 +0800

    Update sampleapp.py

--- sampleapp.py
+++ sampleapp.py
@@ -17,3 +17,13 @@ map_data = pd.DataFrame(
     columns=['lat', 'lon'])
 
 st.map(map_data)
+
+st.header("My first Streamlit App")
+
+st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
+show = st.checkbox('I agree the terms and conditions')
+if show:
+    st.write(pd.DataFrame({
+    'Intplan': ['yes', 'yes', 'yes', 'no'],
+    'Churn Status': [0, 0, 0, 1]
+}))
